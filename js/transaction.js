47de68cd657bed03d2b44942e15e89f612527933
sirazulsihab <smsisihab@gmail.com>
2021-08-26 10:14:40 +0600

    first Commit

--- js/transaction.js
+++ js/transaction.js
@@ -1,61 +1,56 @@
-// Get Input Value Function
-function getInputValue(inputId){
-    const inputFiled = document.getElementById(inputId);
-    const inputText = inputFiled.value;
-    const inputAmount = parseFloat(inputText);
-    inputFiled.value = '';
+function inputMoney(inputField){
+    const input = document.getElementById(inputField);
+    const inputValue = input.value;
+    const inputAmount = parseFloat(inputValue);
+    input.value = '';
     return inputAmount;
 }
-// update Amount Filed Function
-function updateAmountFiled(depositAmount, inputId){
-    const previousDeposit = document.getElementById(inputId);
-    const previousDepositText = previousDeposit.innerText;
-    const previousDepositAmount = parseFloat(previousDepositText);
-    if(depositAmount > 0){
-        previousDeposit.innerText = previousDepositAmount + depositAmount;
+function getInnerText(fieldId){
+    const fieldTag = document.getElementById(fieldId);
+    const fieldText = fieldTag.innerText;
+    const filedAmount = parseFloat(fieldText);
+    return filedAmount;
+}
+function updateTotal(fieldId, amount){
+   /*  const inputFiled = document.getElementById(fieldId);
+    const inputFiledInText = inputFiled.innerText;
+    const inputAmount = parseFloat(inputFiledInText); */
+    const inputAmount = getInnerText(fieldId);
+    const totalAmount = inputAmount + amount;
+    document.getElementById(fieldId).innerText = totalAmount;
+}
+function updateBalance(amount, isAdd){
+    /* const balance = document.getElementById('current-total');
+    const balanceInText = balance.innerText;
+    const balanceAmount = parseFloat(balanceInText);
+     */
+    const balanceAmount = getInnerText('current-total');
+    let totalAmount;
+    if(isAdd){
+        totalAmount = balanceAmount + amount;
+    }else{
+        totalAmount = balanceAmount - amount;
     }
     
-
+    document.getElementById('current-total').innerText = totalAmount;
 }
-// update balance Function
-function updateBalance(depositAmount, isAdd){
-    const balance = document.getElementById('current-total');
-    const balanceText  = balance.innerText;
-    const balanceAmount = parseFloat(balanceText)
-    if(depositAmount > 0){
-        if(isAdd){
-            balance.innerText = balanceAmount + depositAmount;
-        }else{
-            if(balance.innerText < depositAmount){
-                 alert('Sorry! Your Balance Is Low');
-            }else{
-                balance.innerText = balanceAmount - depositAmount;
-            }
-            
-        }
+const depositButton = document.getElementById('deposit-btn');
+depositButton.addEventListener('click', function(){
+    const money = inputMoney('depositField');
+    if(money > 0){
+        updateTotal('current-deposit', money);
+        updateBalance(money, true);
+    }
+
+});
+const widthdrawButton = document.getElementById('widthdraw-btn');
+widthdrawButton.addEventListener('click', function(){
+    const money = inputMoney('widthwrawField');
+    const balance = getInnerText('current-total');
+
+    if(money > 0 && balance >= money){
+        updateTotal('current-widthdraw', money);
+        updateBalance(money, false);
     }
     
-    
-    
-}
-// deposit button area
-const depositBtn = document.getElementById('deposit-btn');
-depositBtn.addEventListener('click', function(){
-    // deposit input value 
-    const depositAmount = getInputValue('deposit')
-    // update deposit amount
-    updateAmountFiled(depositAmount, 'current-deposit');
-    // update Banalace
-    updateBalance(depositAmount, true);
-})
-
-// widthdraw button area
-const widthdrawBtn = document.getElementById('widthdraw-btn')
-widthdrawBtn.addEventListener('click', function (){
-    // widthdraw input value 
-    const widthdrawAmount = getInputValue('widthwraw');
-    // update widthdraw Amount
-    updateAmountFiled(widthdrawAmount, 'current-widthdraw');
-    // update Balance
-    updateBalance(widthdrawAmount, false);   
-})
+});
